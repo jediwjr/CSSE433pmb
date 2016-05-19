440f8b8b0014fb93639b79c0576b92581977bacc
srasp413 <srasp413@gmail.com>
2016-05-18 22:39:23 -0400

    Like function now works

--- pmbProject/pmb/pmbApp/neo4j_functions.py
+++ pmbProject/pmb/pmbApp/neo4j_functions.py
@@ -9,11 +9,12 @@ def create_message(msg_id):
     session.run("CREATE (m:Message {msg_id: {msg_id}})", {"msg_id": msg_id})
 
 def add_like(user,msg):
-    session.run("MATCH (u:User {username:{username}}), (m:Message {msg_id:{msg_id}}) CREATE UNIQUE (u)-[:LIKES]->(m)", {"username":user, "msg_id":msg})
+    session.run("MATCH (u:User {username:{username}}), (m:Message {msg_id:{msg_id}}) CREATE UNIQUE (u)-[r:LIKES]->(m) RETURN r", {"username":user, "msg_id":msg})
 
 def remove_like(user,msg):
     session.run("MATCH (u:User)-[rel:LIKES]->(m:Message) WHERE u.username={username} AND m.msg_id={msg_id} DELETE rel",{"username":user, "msg_id":msg})
 
 def likes(user,msg):
-    result = session.run("MATCH (u:User)-[:LIKES]->(m:Message) WHERE u.username={username} AND m.msg_id={msg_id}",{"username":user, "msg_id":msg})
-    return result != None
+    result = session.run("MATCH (u:User)-[r:LIKES]->(m:Message) WHERE u.username={username} AND m.msg_id={msg_id} RETURN r",{"username":user, "msg_id":msg})
+    result = list(result)
+    return len(result) >= 1
--- pmbProject/pmb/pmbApp/pmbApp.py
+++ pmbProject/pmb/pmbApp/pmbApp.py
@@ -3,6 +3,7 @@ session)
 import uuid
 from cassandra_functions import init_db, view_messages_c, send_message_c, edit_message_c, delete_message_c
 from mongo_functions import add_user, get_user
+from neo4j_functions import create_user, create_message, add_like, likes, remove_like
 import hashlib
 
 app = Flask(__name__)
@@ -52,14 +53,16 @@ def adduser():
             flash('Passwords do not match')
             return redirect(url_for('signup'))
         else:
-            add_user(request.form['username'], request.form['password'])
+            un = request.form['username']
+            add_user(un, request.form['password'])
+            create_user(un)
             return redirect(url_for('.index'))
 
 @app.route('/view_messages', methods=['GET', 'POST'])
 def view_messages():
     rows = view_messages_c()
     returned = [row for row in rows]
-    return render_template('view_messages.html', returned=returned, chunklist=chunkList)
+    return render_template('view_messages.html', returned=returned, chunklist=chunkList, like_func=likes)
 
 @app.route('/edit_message', methods=['GET'])
 def edit_message():
@@ -80,7 +83,9 @@ def post_message():
     text = request.form['text']
     lon = request.form['lon']
     lat= request.form['lat']
-    send_message_c(uuid.uuid4(), text, username, lat, lon)
+    msg_id = uuid.uuid4()
+    send_message_c(msg_id, text, username, lat, lon)
+    create_message(str(msg_id))
     flash('Message Sent')
     return redirect(url_for('send_message'))
 
@@ -93,6 +98,20 @@ def chunkList(l, chunksize):
     for i in range(0, len(l), chunksize):
         chunks.append(l[i:i+chunksize])
     return chunks
+
+@app.route('/like_message', methods=['GET', 'POST'])
+def like_message():
+    username = session['username']
+    msg = request.form['msg_id']
+    add_like(username,msg)
+    return redirect(url_for('view_messages'))
+
+@app.route('/unlike_message', methods=['GET','POST'])
+def unlike_message():
+    username = session['username']
+    msg = request.form['msg_id']
+    remove_like(username,msg)
+    return redirect(url_for('view_messages'))
 
 if __name__ == '__main__':
     app.secret_key = 'secret key'
